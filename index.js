ef4b9c927c5d76cc48f82217face2d596f16e93b
Anfer Valera <anfervalera11@gmail.com>
2021-06-15 12:33:09 -0400

    Finalizada la version mobile

---
+++ index.js
@@ -0,0 +1,16 @@
+let hamburguer = document.getElementById("hamburguer__img")
+let list = document.getElementById("hamburguer__list")
+
+function toogleClass() {
+    if (list.classList.contains("hide")) {
+        list.classList.remove("hide")
+        list.classList.add("hamburger__list")
+        hamburguer.classList.add("close")
+
+    } else {
+        list.classList.add("hide")
+        list.classList.remove("hamburger__list")
+        hamburguer.classList.remove("close")
+
+    }
+}
